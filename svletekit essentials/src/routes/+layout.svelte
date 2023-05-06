4e619364d64e96a4eae006d91098d27613cedcbb
LsrkMateo <121040035+LsrkMateo@users.noreply.github.com>
2023-05-06 10:49:10 -0500

    Add basic structure to the page

--- svletekit essentials/src/routes/+layout.svelte
+++ svletekit essentials/src/routes/+layout.svelte
@@ -2,15 +2,13 @@
 	import '../app.css';
 </script>
 
-<nav>
-	<ul>
-		<li>
-			<a href="/">Home</a>
-		</li>
-		<li>
-			<a href="/posts">Posts</a>
-		</li>
-	</ul>
-</nav>
-
+<article id="titulo-inicial">
+	<header>
+		<h1 style="margin: 0;">Sveltekit CRUD application</h1>
+	</header>
+	<p>
+		Test aplication for crud operations using prisma orm library, try yourself to create an article
+		and start to make changes on it!
+	</p>
+</article>
 <slot />
--- svletekit essentials/src/routes/+page.svelte
+++ svletekit essentials/src/routes/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Hello :D</h1>
+<div class="grid">
+	<div>
+		<h2>Articles:</h2>
+		<article>
+			<header>Example article</header>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorum at eius, culpa
+				libero fugit dolorem nihil eaque ratione ullam saepe totam numquam error magnam maiores
+				dignissimos asperiores quos? Est!
+			</p>
+			<button class="outline secondary">Delete Article </button>
+			<a href="/" role="button" class="outline contrast" style="width: 100%;">Edit article </a>
+		</article>
+	</div>
+	<form action="" method="post">
+		<h3>New article</h3>
+		<label for="title">title</label>
+		<input type="text" id="title" name="title" />
+		<label for="title">title</label>
+		<textarea name="content" id="content" rows={5} />
+		<button type="submit"> Add article </button>
+	</form>
+</div>
